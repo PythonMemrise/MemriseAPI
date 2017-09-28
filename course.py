fb649809513cc98ef520911ef266442bc35cc093
PythonMemrise <32368318+PythonMemrise@users.noreply.github.com>
2017-09-28 18:41:11 +0200

    Update course.py

--- course.py
+++ course.py
@@ -9,10 +9,6 @@ __all__ = ['actuality', 'allcourses', 'courseinfo', 'coursenumber', 'coursetest'
 
 actuality = coerce(1.0, 9.2017) #version: 1.0 last, last update: September 2017
 
-if sys.version[0] == "2":
-    VER = 2
-else:
-    VER = 3
 
 def linenumber(url, text): #gets the number of a line
     myData = urllib.urlopen(url)   
@@ -41,10 +37,7 @@ def coursenumber():
         start = 1580000
         for i in range(1, 20000, 1000):
             nstart = start + i
-            if VER == 2:
-                print nstart
-            else:
-                print(nstart)
+            print nstart
             url = "http://www.memrise.com/course/" + str(nstart)
             if "<h2>The page you are trying to get to doesn't exist.</h2>" in urllib.urlopen(url).read():
                 return "Current course: " + nstart
@@ -104,18 +97,12 @@ def allcourses(course_info=False):
     if course_info == False:
         for i in range(1600000):
             if coursetest(i) == True:
-                if VER == 2:
                     print i
-                else:
-                    print(i)
     if course_info == True:
         for i in range(1600000):
             try:
                 if coursetest(i) == True:
-                    if VER == 2:                    
                         print str(i), courseinfo(i)
-                    else:
-                        print(str(i), courseinfo(i))
             except KeyboardInterrupt:
                 break
             except IOError:
