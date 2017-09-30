898189ce0d35ad0be700ae0c763c549fc9771b65
PythonMemrise <32368318+PythonMemrise@users.noreply.github.com>
2017-09-30 13:58:49 +0200

    Updated

--- mem_exceptions.py
+++ mem_exceptions.py
@@ -1,15 +1,4 @@
 
-class NoMems(Exception):
-    def __init__(self):
-        self.msg = "This user hasn't any mems!"
-    def __str__(self):
-        return self.msg
-
-class NoPoints(Exception):
-    def __init__(self):
-        self.msg = "This user hasn't any points!"
-    def __str__(self):
-        return self.msg
 
 class NoConnection(Exception):
     def __init__(self):
@@ -17,32 +6,12 @@ class NoConnection(Exception):
     def __str__(self):
         return self.msg
    
-class NoFollowing(Exception):
-    def __init__(self):
-        self.msg = "This user doesn't follow anybody!"
-    def __str__(self):
-        return self.msg
-    
-class NoFollower(Exception):
-    def __init__(self):
-        self.msg = "There aren't followers!"
-    def __str__(self):
-        return self.msg
-
-class NoWords(Exception):
-    def __init__(self):
-        self.msg = "This user didn't learn any words!"
-    def __str__(self):
-        return self.msg
-    
 class UserError(Warning):
     def __init__(self):
         self.msg = "This user doesn't exist!"
     def __str__(self):
         return self.msg
    
-
-
 class CourseError(Exception):
     pass
 
