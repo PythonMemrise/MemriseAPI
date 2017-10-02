ea2ecbace7b3e5b9f40828f4272e9a52be0a745c
PythonMemrise <32368318+PythonMemrise@users.noreply.github.com>
2017-10-02 11:07:20 +0200

    Updated
    Version 1.2, better errors

--- mem_exceptions.py
+++ mem_exceptions.py
@@ -1,8 +1,11 @@
+#MemriseAPI
+actuality = coerce(1.2, 9.2017) #version: 1.2 last, last update: October 2017
 
+__all__ = ['actuality', 'CourseError', 'NoConnection', 'UserError']
 
 class NoConnection(Exception):
     def __init__(self):
-        self.msg = "There is no connection!"
+        self.msg = "Can't connect to memrise.com!"
     def __str__(self):
         return self.msg
    
